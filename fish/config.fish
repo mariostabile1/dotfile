f2ac438806e09cb2c804e54fcc452a96f52fe790
mariostabile1 <66466058+mariostabile1@users.noreply.github.com>
2024-09-17 18:27:25 +0200

    pull 17-09-2024

--- fish/config.fish
+++ fish/config.fish
@@ -5,6 +5,13 @@ end
 # Created by `pipx` on 2023-09-01 21:27:20
 set PATH $PATH /home/mario/.local/bin
 
+# Custom funtions
+function lfcd --wraps="lf" --description="lf - Terminal file manager (changing directory on exit)"
+    # command is needed in case lfcd is aliased to lf.
+    # Quotes will cause cd to not change directory if lf prints nothing to stdout due to an error.
+    cd "$(command lf -print-last-dir $argv)"
+end
+
 # Alias
 alias nt "nautilus ."
 alias pof "sync && poweroff"
@@ -13,6 +20,8 @@ alias push "pushd ."
 alias pop "popd"
 alias lock "xdg-screensaver lock"
 alias btop "bpytop"
+alias lf lfcd
+
 
 # Exa file listener config
 if type -q exa
